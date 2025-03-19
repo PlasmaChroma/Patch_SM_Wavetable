af60aafda1ae3cdc70304e395ecafdfe0b225447
Levi Kendall <plasmachroma@gmail.com>
2025-03-18 20:45:25 -0400

    Remove some unused code, allow pitch and WT selection via patch points

--- WaveTable.cpp
+++ WaveTable.cpp
@@ -1,6 +1,5 @@
 #include "daisy_patch_sm.h"
 #include "daisysp.h"
-#include "WaveTableOsc.h"
 #include "wtosc2.h"
 #include "WavHeader.h"
 #include "SampleVoice.h"
@@ -13,7 +12,6 @@ using namespace daisysp;
 DaisyPatchSM hw;
 CpuLoadMeter loadMeter;
 
-WaveTableOsc* wtOsc;
 WTOSC2* wtOsc2;
 
 // for loading WT from sd card
@@ -32,181 +30,66 @@ FIL            SDFile;
 // buffer is twice as big as expected WT size, incase of stereo?
 int16_t DSY_SDRAM_BSS wt_sdram_buffer[1024 * 512];
 int wt_row_count = 0;
-
-void fft(int N, myFloat *ar, myFloat *ai)
-/* in-place complex fft */
-{    
-    int i, j, k, L;            /* indexes */
-    int M, TEMP, LE, LE1, ip;  /* M = log N */
-    int NV2, NM1;
-    myFloat t;               /* temp */
-    myFloat Ur, Ui, Wr, Wi, Tr, Ti;
-    myFloat Ur_old;
-    
-    // if ((N > 1) && !(N & (N - 1)))   // make sure we have a power of 2
-    
-    NV2 = N >> 1;
-    NM1 = N - 1;
-    TEMP = N; /* get M = log N */
-    M = 0;
-    while (TEMP >>= 1) ++M;
-    
-    /* shuffle */
-    j = 1;
-    for (i = 1; i <= NM1; i++) {
-        if(i<j) {             /* swap a[i] and a[j] */
-            t = ar[j-1];     
-            ar[j-1] = ar[i-1];
-            ar[i-1] = t;
-            t = ai[j-1];
-            ai[j-1] = ai[i-1];
-            ai[i-1] = t;
-        }
-        
-        k = NV2;             /* bit-reversed counter */
-        while(k < j) {
-            j -= k;
-            k /= 2;
-        }
-        
-        j += k;
-    }
-    
-    LE = 1.;
-    for (L = 1; L <= M; L++) {            // stage L
-        LE1 = LE;                         // (LE1 = LE/2) 
-        LE *= 2;                          // (LE = 2^L)
-        Ur = 1.0;
-        Ui = 0.; 
-        Wr = cos(M_PI/(float)LE1);
-        Wi = -sin(M_PI/(float)LE1); // Cooley, Lewis, and Welch have "+" here
-        for (j = 1; j <= LE1; j++) {
-            for (i = j; i <= N; i += LE) { // butterfly
-                ip = i+LE1;
-                Tr = ar[ip-1] * Ur - ai[ip-1] * Ui;
-                Ti = ar[ip-1] * Ui + ai[ip-1] * Ur;
-                ar[ip-1] = ar[i-1] - Tr;
-                ai[ip-1] = ai[i-1] - Ti;
-                ar[i-1]  = ar[i-1] + Tr;
-                ai[i-1]  = ai[i-1] + Ti;
-            }
-            Ur_old = Ur;
-            Ur = Ur_old * Wr - Ui * Wi;
-            Ui = Ur_old * Wi + Ui * Wr;
-        }
-    }
-}
-
-//
-// if scale is 0, auto-scales
-// returns scaling factor (0.0 if failure), and wavetable in ai array
-//
-float makeWaveTable(WaveTableOsc *osc, int len, myFloat *ar, myFloat *ai, myFloat scale, double topFreq) {
-    fft(len, ar, ai);
-    
-    if (scale == 0.0) {
-        // calc normal
-        myFloat max = 0;
-        for (int idx = 0; idx < len; idx++) {
-            myFloat temp = fabs(ai[idx]);
-            if (max < temp)
-                max = temp;
-        }
-        scale = 1.0 / max * .999;        
-    }
-    
-    // normalize
-    float wave[len];
-    for (int idx = 0; idx < len; idx++)
-        wave[idx] = ai[idx] * scale;
-        
-    if (osc->addWaveTable(len, wave, topFreq))
-        scale = 0.0;
-    
-    return scale;
-}
-
-void defineSawtooth(int len, int numHarmonics, myFloat *ar, myFloat *ai) {
-    if (numHarmonics > (len >> 1))
-        numHarmonics = (len >> 1);
-    
-    // clear
-    for (int idx = 0; idx < len; idx++) {
-        ai[idx] = 0;
-        ar[idx] = 0;
-    }
-
-    // sawtooth
-    for (int idx = 1, jdx = len - 1; idx <= numHarmonics; idx++, jdx--) {
-        myFloat temp = -1.0 / idx;
-        ar[idx] = -temp;
-        ar[jdx] = temp;
-    }
-}
-
-void setSawtoothOsc(WaveTableOsc *osc, float baseFreq) {    
-    // calc number of harmonics where the highest harmonic baseFreq and lowest alias an octave higher would meet
-    int maxHarms = sampleRate / (3.0 * baseFreq) + 0.5;
-
-    // round up to nearest power of two
-    unsigned int v = maxHarms;
-    v--;            // so we don't go up if already a power of 2
-    v |= v >> 1;    // roll the highest bit into all lower bits...
-    v |= v >> 2;
-    v |= v >> 4;
-    v |= v >> 8;
-    v |= v >> 16;
-    v++;            // and increment to power of 2
-    int tableLen = v * 2 * overSamp;  // double for the sample rate, then oversampling
-
-    myFloat ar[tableLen], ai[tableLen];   // for ifft
-
-    double topFreq = baseFreq * 2.0 / sampleRate;
-    myFloat scale = 0.0;
-    for (; maxHarms >= 1; maxHarms >>= 1) {
-        defineSawtooth(tableLen, maxHarms, ar, ai);
-        scale = makeWaveTable(osc, tableLen, ar, ai, scale, topFreq);
-        topFreq *= 2;
-        if (tableLen > constantRatioLimit) // variable table size (constant oversampling but with minimum table size)
-            tableLen >>= 1;
-    }
-}
-
-
 float outputValue = 0;
 float freq_with_offset = 0;
+
+float pitchInput = 0;
+float pitchFrequency = 0;
+float pitchVoltage = 0;
+float freq_offset = 0;
+// pitch range for 0-5V input CV
+const double C1_FREQ = 32.70319566;
+const double C6_FREQ = 1046.50226112;
+
+double voltageToFrequency(double voltage) {
+    // Clamp input voltage between 0V and 5V
+    if (voltage < 0) voltage = 0;
+    if (voltage > 5) voltage = 5;
+    
+    const double baseFreq = C1_FREQ;
+    
+    // In the 1V/oct standard, each volt represents an octave (frequency doubling)
+    // For 5V range, we cover exactly 5 octaves from C0 to C5
+    // Calculate the frequency using the exponential relationship
+    if (voltage <= 0.04) return C1_FREQ; // thresholding to prevent offset doing weird thingsB
+    double frequency = baseFreq * pow(2, voltage + freq_offset);
+    
+    return frequency;
+}
+
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
+    // performance monitoring start
     loadMeter.OnBlockStart();
-	static int cycle = 0;
 
 	hw.ProcessAllControls();
-	float macroTune = hw.GetAdcValue(CV_1);
-	float freq = fmap(macroTune, 20.f, 15000.f, Mapping::LOG);
-#if 0    
-	if (cycle % 10000 == 0) 
-    {
-        FixedCapStr<16> str("F: ");
-        str.AppendFloat(freq);
-        hw.PrintLine(str);
-    }
-#endif
+	//float macroTune = hw.GetAdcValue(CV_1);
+	//float freq = fmap(macroTune, 20.f, 15000.f, Mapping::LOG);
+
 	float microTune = hw.GetAdcValue(CV_2);
-	float freq_offset = fmap(microTune, -500.f, 500.f, Mapping::LINEAR);
+	freq_offset = fmap(microTune, -0.08f, 0.08f, Mapping::LINEAR);
     	
 	// constrain potential frequencies regardless
+    #if 0
 	freq_with_offset = freq + freq_offset;
 	if (freq_with_offset < 20) freq_with_offset = 20;
 	if (freq_with_offset > 15000) freq_with_offset = 15000;
-
-    double freqVal = freq_with_offset / sampleRate;
+    #endif
 
     float tableTune= hw.GetAdcValue(CV_3);
     int tableRowSet = (int)fmap(tableTune, 0, 256, Mapping::LINEAR);
 
+    pitchInput= hw.GetAdcValue(CV_5);
+    pitchVoltage = fmap(pitchInput, 0, 5, Mapping::LINEAR);
+    pitchFrequency = voltageToFrequency(pitchVoltage);
+    double freqVal = (pitchFrequency) / sampleRate;
+
+    float wtIndex = hw.GetAdcValue(CV_6);
+    wtIndex = fmap(wtIndex, 0, wt_row_count, Mapping::LINEAR);
+
 	for (size_t i = 0; i < size; i++)
 	{		
-		wtOsc2->setCurrentRow(tableRowSet);
+		wtOsc2->setCurrentRow(wtIndex);
 		wtOsc2->setFrequency(freqVal);
         outputValue = wtOsc2->getOutput();      
 		OUT_L[i] = outputValue;
@@ -214,7 +97,6 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
 		wtOsc2->updatePhase();
 	}
     
-    cycle++;
     loadMeter.OnBlockEnd();
 }
 
@@ -227,9 +109,8 @@ int main(void)
     loadMeter.Init(hw.AudioSampleRate(), hw.AudioBlockSize());
     hw.PrintLine("Starting WaveTable Daisy Patch SM");
 
-	wtOsc = new WaveTableOsc();
     wtOsc2 = new WTOSC2();
-	setSawtoothOsc(wtOsc, baseFrequency);    // ----- Get the wavetable data from the SD card -----
+	// ----- Get the wavetable data from the SD card -----
     // Init SD Card
     SdmmcHandler::Config sd_cfg;
     sd_cfg.Defaults();
@@ -251,6 +132,17 @@ int main(void)
     hw.PrintLine("Loaded wavetable with %d rows", wt_row_count);
     // ----- (end)  -----
 
+    #if 0
+    // Create an ADC Channel Config object
+    AdcChannelConfig adc_config;
+    // Set up the ADC config with a connection to pin A0
+    adc_config.InitSingle(daisy::patch_sm::DaisyPatchSM::C6);
+    // Initialize the ADC peripheral with that configuration
+    hw.adc.Init(&adc_config, 1);
+    // Start the ADC
+    hw.adc.Start();
+    #endif
+
     hw.PrintLine("Starting Audio Callback");
 	hw.StartAudio(AudioCallback);
 	while(1) {
@@ -265,13 +157,18 @@ int main(void)
         hw.PrintLine("  Min: " FLT_FMT3, FLT_VAR3(minLoad * 100.0f));
 
         {
-            FixedCapStr<32> str("O: ");
-            str.AppendFloat(outputValue);
+            FixedCapStr<32> str("V: ");
+            str.AppendFloat(pitchVoltage);
             hw.Print(str);
         }
         {
+            FixedCapStr<32> str("   Pi: ");
+            str.AppendFloat(pitchInput);
+            hw.Print(str);
+        }        
+        {
             FixedCapStr<32> str("  F+: ");
-            str.AppendFloat(freq_with_offset);
+            str.AppendFloat(pitchFrequency);
             hw.PrintLine(str);
         }
         // don't spam the serial connection too much
